086651970546a8c987d88d0a1decf9fcaa4d99dc
João Maia <15joaomaia@gmail.com>
2022-03-15 23:42:47 -0300

    refazendo o projeto do react app com o vite js

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/sass/style.scss'
+import App from './App'
+import 'bulma'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
